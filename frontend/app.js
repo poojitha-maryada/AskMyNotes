940789425f5630b3c478ba9c2cd4dfad4f7b999a
poojitha-maryada <poojithamaryada@gmail.com>
2026-05-15 10:09:11 +0530

    Updated frontend files

---
+++ frontend/app.js
@@ -0,0 +1,79 @@
+const pdfInput = document.getElementById("pdf-input");
+const uploadStatus = document.getElementById("upload-status");
+
+const questionInput = document.getElementById("question");
+const askBtn = document.getElementById("ask-btn");
+
+const answerBox = document.getElementById("answer");
+const answerText = document.getElementById("answer-text");
+
+const statusText = document.getElementById("status");
+
+let uploadedFileName = "";
+
+// PDF Upload
+pdfInput.addEventListener("change", function () {
+
+    if (pdfInput.files.length > 0) {
+
+        uploadedFileName = pdfInput.files[0].name;
+
+        uploadStatus.textContent =
+            `Uploaded: ${uploadedFileName}`;
+
+    } else {
+
+        uploadStatus.textContent =
+            "No file uploaded yet.";
+    }
+});
+
+// Submit Button
+askBtn.addEventListener("click", function () {
+
+    const question = questionInput.value.trim();
+
+    // Clear old messages
+    statusText.textContent = "";
+    answerBox.hidden = true;
+
+    // Default color
+    statusText.style.color = "red";
+
+    // If no PDF uploaded
+    if (uploadedFileName === "") {
+
+        statusText.textContent =
+            "Please upload a PDF.";
+
+        return;
+    }
+
+    // If no question entered
+    if (question === "") {
+
+        statusText.textContent =
+            "Please enter the question.";
+
+        return;
+    }
+
+    // Show thinking message
+    statusText.style.color = "gray";
+    statusText.textContent = "Thinking...";
+
+    // Delay before showing answer
+    setTimeout(function () {
+
+        // Clear thinking text
+        statusText.textContent = "";
+
+        // Show answer box
+        answerBox.hidden = false;
+
+        // Display answer
+        answerText.textContent =
+            `The answer for "${question}" is`;
+
+    }, 2000); // 2 seconds delay
+});
